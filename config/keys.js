1de97a9f17ffee4df3e8f0e3a438c04602ed25d5
yasir <yasir@yasirs-MacBook-Pro.local>
2018-10-28 00:24:59 +0500

    first commit

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://yasir:yasir@ds163769.mlab.com:63769/devconnector',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://yasir2:yasir2@ds131313.mlab.com:31313/userconnector',
+  secretOrKey: 'secret'
+};
+
